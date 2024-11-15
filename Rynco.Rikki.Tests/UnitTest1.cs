299a94d8b88e1fadc32d5a571cc08477c3ad4d3b
Rynco Maekawa <i@rynco.me>
2024-11-15 19:03:20 +0800

    Add tests

---
+++ Rynco.Rikki.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Rynco.Rikki.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
